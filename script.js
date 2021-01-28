830b25db59daf7cd0aef4d3551423ae1e14446aa
darilag2 <daryl_darilag@yahoo.co.uk>
2021-01-28 18:12:43 +0000

    script.js changed

--- script.js
+++
@@ -1,8 +0,0 @@
-function classToggle() {
-    const navs = document.querySelectorAll('.Navbar-Items')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar-ToggleShow'));
-  }
-  
-  document.querySelector('.Navbar-Link-toggle')
-    .addEventListener('click', classToggle)
